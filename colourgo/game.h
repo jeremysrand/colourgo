df3238358a778b906580e81c18168801bb32c475
Jeremy Rand <jeremy@rand-family.com>
2017-07-19 10:28:12 -0500

    Lots of hires boilerplate.  Startup, clear hires, show hires and wait for a keypress before quitting.

---
+++ colourgo/game.h
@@ -0,0 +1,16 @@
+//
+//  game.h
+//  colourgo
+//
+//  Created by Jeremy Rand on 2017-07-19.
+//  Copyright © 2017 Jeremy Rand. All rights reserved.
+//
+
+#ifndef __colourgo__game__
+#define __colourgo__game__
+
+
+extern void game(void);
+
+
+#endif /* defined(__colourgo__game__) */
---
+++ colourgo/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  colourgo
+//
+//  Created by Jeremy Rand on 2017-07-19.
+//  Copyright © 2017 Jeremy Rand. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "game.h"
+
+
+int main(void)
+{
+    game();
+    return 0;
+}
